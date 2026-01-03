d4dc5cae409592d14e31e94acfceb2846f35c1ce
marsion77 <marison399@gmail.com>
2026-01-03 18:41:15 +0530

    Just Installed Razor for payments

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const menuRouter = require("./Router/menuRouter")
 const quantityRouter = require("./Router/quantityRouter")
 const orderRouer = require("./Router/orderRouter")
 const wishlistRouter = require("./Router/wishlistRouter")
+import cartRoutes from './Model/cart.js';
+import paymentRoutes from './routes/payment.js';
+import cartController from '../controllers/cartController.js';
+import paymentController from '../controllers/paymentController.js';
+
+app.use('/api/cart', cartRoutes);
+app.use('/api/payment', paymentRoutes);
+
 
 app.use("/users", userRouter) 
 app.use("/category", categoryRouter)
@@ -21,6 +29,16 @@ app.use("/menu",menuRouter)
 app.use("/quantity", quantityRouter)
 app.use("/order",orderRouer)
 app.use("/wishlist", wishlistRouter)
+
+
+
+// router.post('/add/:menuItemId', auth,cartController.add);
+// router.get('/', auth, cartController.getCart);
+// router.delete('/remove/:menuItemId', auth, cartController.removeFromCart);
+
+
+// router.post('/create-order', auth, paymentController.createOrder);
+// router.post('/verify', auth, paymentController.verifyPayment);
 
 database.on('open',()=>{
     app.listen(9080,()=>{
